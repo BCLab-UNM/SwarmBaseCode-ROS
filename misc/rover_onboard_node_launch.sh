5a457427d29039dedfea79c586c88d34cc68066d
Joshua Hecker <jhecker@gmail.com>
2015-12-08 16:53:25 -0700

    Add mobility shutdown messsage to physical robot run script

--- misc/rover_onboard_node_launch.sh
+++ misc/rover_onboard_node_launch.sh
@@ -68,11 +68,12 @@ while true; do
     read choice;
 
     if [ "$choice" == "q" ];then
+	rosnode kill $HOSTNAME\_MOBILITY
+	rostopic pub -1 /$HOSTNAME\/mobility geometry_msgs/Twist '{linear: {x: 0.0, y: 0.0, z: 0.0}, angular: {x: 0.0, y: 0.0, z: 0.0}}'
+	rosnode kill $HOSTNAME\_ABRIDGE
 	rosnode kill $HOSTNAME\_NAVSAT
 	rosnode kill $HOSTNAME\_EKF
-	rosnode kill $HOSTNAME\_ABRIDGE
 	rosnode kill $HOSTNAME\_CAMERA
-	rosnode kill $HOSTNAME\_MOBILITY
 	rosnode kill $HOSTNAME\_OBSTACLE
 	rosnode kill $HOSTNAME\_TARGET
 	rosnode kill ublox_gps
