235a88f28e9770c9d1f4929ec34d794374571e6b
Antonio Griego <deano505@unm.edu>
2016-07-22 17:51:56 -0600

    Updated the GripperPlugin class to include more standardized debug output using ROS_DEBUG(). In addition, an optional setting accessible through a rover's model SDF file has been added that can toggle this debug feature on and off.

--- src/gazebo_plugins/src/GripperPlugin/GripperPlugin.h
+++ src/gazebo_plugins/src/GripperPlugin/GripperPlugin.h
@@ -3,63 +3,79 @@
 
 #include <gazebo/gazebo.hh>
 #include <gazebo/physics/physics.hh>
-#include <gazebo/msgs/msgs.hh>
-#include <boost/bind.hpp>
 #include <ros/ros.h>
 #include <ros/callback_queue.h>
-#include <ros/subscribe_options.h>
 #include <std_msgs/Float32.h>
 #include <thread>
-#include <iostream>
-#include <string>
 #include "GripperManager.h"
 
+/**
+ * This class implements a gripper plugin for the NASA Swarmathon Rovers.
+ *
+ * <p>The gripper design consists of a wrist which rotates the gripper up or
+ * down about a fixed axis. The gripper itself is composed of two fingers which
+ * open and close symmetrically.
+ *
+ * <p>The gripper is designed to use a base PID controller for each of the
+ * three primary joints. A gripper manager is implemented to pass force and
+ * angle data between this class and the PID controllers.
+ *
+ * @author Matthew Fricke
+ * @author Antonio Griego
+ * @see    ModelPlugin
+ * @see    GripperManager
+ * @see    PIDController
+ */
 namespace gazebo {
+    class GripperPlugin : public ModelPlugin {
 
-class GripperPlugin : public ModelPlugin {
+    public:
 
-  public:
+        // required overloaded function from ModelPlugin class
+        void Load(gazebo::physics::ModelPtr parent, sdf::ElementPtr sdf);
 
-    // required overloaded function from ModelPlugin class
-    void Load(gazebo::physics::ModelPtr parent, sdf::ElementPtr sdf);
+        // Gazebo actuation function
+        void updateWorldEventHandler();
 
-    // Gazebo actuation function
-    void updateWorldEventHandler();
+        // ROS topic handlers
+        void setWristAngleHandler(const std_msgs::Float32ConstPtr &msg);
+        void setFingerAngleHandler(const std_msgs::Float32ConstPtr &msg);
 
-    // ROS topic handlers
-    void setWristAngleHandler(const std_msgs::Float32ConstPtr &msg);
-    void setFingerAngleHandler(const std_msgs::Float32ConstPtr &msg);
+    private:
 
-  private:
+        // private helper functions
+        void processRosQueue();
+        physics::JointPtr loadJoint(std::string jointTag);
 
-    void processRosQueue();
-    gazebo::physics::JointPtr loadJoint(std::string jointTag);
+        // pointers to gazebo model and xml configuration file
+        physics::ModelPtr model;
+        sdf::ElementPtr sdf;
 
-    physics::ModelPtr model;
-    sdf::ElementPtr sdf;
+        // interface for processing ROS message queue
+        event::ConnectionPtr updateConnection;
+        std::unique_ptr<ros::NodeHandle> rosNode;
+        std::thread rosQueueThread;
+        ros::CallbackQueue rosQueue;
 
-    // interface for processing ROS message queue
-    event::ConnectionPtr updateConnection;
-    std::unique_ptr<ros::NodeHandle> rosNode;
-    std::thread rosQueueThread;
-    ros::CallbackQueue rosQueue;
+        // ROS subscribers
+        ros::Subscriber wristAngleSubscriber;
+        ros::Subscriber fingerAngleSubscriber;
 
-    // ROS subscribers
-    ros::Subscriber wristAngleSubscriber;
-    ros::Subscriber fingerAngleSubscriber;
+        // gripper component objects
+        GripperManager gripperManager;
+        physics::JointPtr wristJoint;
+        physics::JointPtr leftFingerJoint;
+        physics::JointPtr rightFingerJoint;
+        math::Angle desiredWristAngle;
+        math::Angle desiredFingerAngle;
 
-    // gripper component objects
-    GripperManager gripperManager;
-    gazebo::physics::JointPtr wristJoint;
-    gazebo::physics::JointPtr leftFingerJoint;
-    gazebo::physics::JointPtr rightFingerJoint;
+        common::Time previousUpdateTime;
+        float updateRateInSeconds;
+        bool isDebuggingModeActive;
+    };
 
-    math::Angle desiredWristAngle;
-    math::Angle desiredFingerAngle;
-};
-
-// Register this plugin with the simulator
-GZ_REGISTER_MODEL_PLUGIN(GripperPlugin)
-  }
+    // Register this plugin with the simulator
+    GZ_REGISTER_MODEL_PLUGIN(GripperPlugin)
+}
 
 #endif /* GRIPPER_PLUGIN_H */
