49cdbb48672c43d86d7140200048029b159e3a33
Antonio Griego <deano505@unm.edu>
2017-01-13 13:07:00 -0700

    cleaned comments and code formatting for the CameraFrame class

--- src/rqt_rover_gui/src/CameraFrame.cpp
+++ src/rqt_rover_gui/src/CameraFrame.cpp
@@ -1,93 +1,84 @@
 #include <CameraFrame.h>
 
-namespace rqt_rover_gui
-{
+namespace rqt_rover_gui {
 
 CameraFrame::CameraFrame(QWidget *parent, Qt::WFlags flags) : QFrame(parent)
 {
-    connect(this, SIGNAL(delayedUpdate()), this, SLOT(update()), Qt::QueuedConnection);
+  connect(this, SIGNAL(delayedUpdate()), this, SLOT(update()),
+          Qt::QueuedConnection);
 
-        frames = 0;
+  frames = 0;
 }
 
-void CameraFrame::paintEvent(QPaintEvent* event)
-{
-    QPainter painter(this);
-    painter.setPen(Qt::white);
+void CameraFrame::paintEvent(QPaintEvent* event) {
+  QPainter painter(this);
+  painter.setPen(Qt::white);
 
+  image_update_mutex.lock();
 
-    image_update_mutex.lock();
-    if (!(image.isNull()))
-    {
-        //painter.drawText(QPoint(50,50), "Image Received From Camera");
+  if (!(image.isNull())) {
+    painter.drawImage(contentsRect(), image);
 
-        painter.drawImage(contentsRect(), image);
+    if(target_corners_1.size() > 0) {
+      QPen pen(Qt::red);
+      pen.setWidth(2);
+      painter.setPen(pen);
 
-        if(target_corners_1.size() > 0) 
-        {
-            QPen pen(Qt::red);
-            pen.setWidth(2);
-            painter.setPen(pen);
+      for(int i = 0; i < target_corners_1.size(); i++)  {
+        QPointF points[4] = {
+          QPointF(target_corners_1[i].first, target_corners_1[i].second),
+          QPointF(target_corners_2[i].first, target_corners_2[i].second),
+          QPointF(target_corners_3[i].first, target_corners_3[i].second),
+          QPointF(target_corners_4[i].first, target_corners_4[i].second),
+        };
 
-            for(int i = 0; i < target_corners_1.size(); i++) 
-            {
-                QPointF points[4] = {
-                    QPointF(target_corners_1[i].first, target_corners_1[i].second),
-                    QPointF(target_corners_2[i].first, target_corners_2[i].second),
-                    QPointF(target_corners_3[i].first, target_corners_3[i].second),
-                    QPointF(target_corners_4[i].first, target_corners_4[i].second),
-                };
+        painter.drawPolygon(points, 4);
+      }
 
-                painter.drawPolygon(points, 4);
-            }
-
-            painter.setPen(Qt::white);
-
-        }
-
-        target_corners_1.clear();
-        target_corners_2.clear();
-        target_corners_3.clear();
-        target_corners_4.clear();
-
-    }
-    else
-    {
-
-        painter.drawText(QPoint(50,50), "No Image Received From Camera");
-    }
-    image_update_mutex.unlock();
-
-    // Track the frames per second for development purposes
-    QString frames_per_second;
-    frames_per_second = QString::number(frames /(frame_rate_timer.elapsed() / 1000.0), 'f', 0) + " FPS";
-
-    QFontMetrics fm(painter.font());
-    painter.drawText(this->width()-fm.width(frames_per_second), fm.height(), frames_per_second);
-
-     frames++;
-
-    if (!(frames % 100)) // time how long it takes to dispay 100 frames
-    {
-        frame_rate_timer.start();
-        frames = 0;
+      painter.setPen(Qt::white);
     }
 
-    // end frames per second
+    target_corners_1.clear();
+    target_corners_2.clear();
+    target_corners_3.clear();
+    target_corners_4.clear();
+  } else {
+    painter.drawText(QPoint(50,50), "No Image Received From Camera");
+  }
 
+  image_update_mutex.unlock();
+
+  // Track the frames per second for development purposes
+  float fps = (float)frames / ((float)frame_rate_timer.elapsed() / 1000.0);
+  QString frames_per_second = QString::number(fps, 'f', 0) + " FPS";
+
+  QFontMetrics fm(painter.font());
+  painter.drawText(this->width()-fm.width(frames_per_second), fm.height(),
+                   frames_per_second);
+
+  frames++;
+
+  // time how long it takes to dispay 100 frames
+  if (!(frames % 100)) {
+    frame_rate_timer.start();
+    frames = 0;
+  }
+
+  // end frames per second
 }
 
-void CameraFrame::setImage(const QImage& img)
-{
+void CameraFrame::setImage(const QImage& img) {
     image_update_mutex.lock();
     image = img.copy();
     image_update_mutex.unlock();
     emit delayedUpdate();
 }
 
-void CameraFrame::addTarget(std::pair<double,double> c1, std::pair<double,double> c2, std::pair<double,double> c3,
-        std::pair<double,double> c4, std::pair<double,double> center)
-{
+void CameraFrame::addTarget(std::pair<double,double> c1,
+                            std::pair<double,double> c2,
+                            std::pair<double,double> c3,
+                            std::pair<double,double> c4,
+                            std::pair<double,double> center) {
     target_corners_1.push_back(c1);
     target_corners_2.push_back(c2);
     target_corners_3.push_back(c3);
@@ -95,5 +86,5 @@ void CameraFrame::addTarget(std::pair<double,double> c1, std::pair<double,double
     target_centers.push_back(center);
 }
 
-}
+} /* END: namespace rqt_rover_gui */
 
