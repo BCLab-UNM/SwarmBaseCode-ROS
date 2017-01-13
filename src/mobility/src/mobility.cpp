345e535cb9d42593ef7e70ab704626d8450f9fa8
Jarett Jones <jch_Jones@ymail.com>
2017-01-13 12:11:38 -0700

    Added custom transform from map to odom
    This transform assume that map and odom share the same orientation at all times and that map and odom have the same units of measuremeant for distance ie no nonlinearity when moveing. It is a stop gap untill issues of rotation between map and odom can be resolved.

--- src/mobility/src/mobility.cpp
+++ src/mobility/src/mobility.cpp
@@ -228,6 +228,11 @@ void mobilityStateMachine(const ros::TimerEvent&) {
 
     mapAverage(); //calls the averaging function, also responsible for transform from Map frame to odom frame.
 
+   stringstream ss;
+   ss << "map center " << centerLocationMap.x << " : " << centerLocationMap.y << " centerLocation " << centerLocation.x << " : " << centerLocation.y << " currentLlocation " << currentLocation.x << " : " << currentLocation.y << " currentLocationAverage " << currentLocationAverage.x << " : " << currentLocationAverage.y << " curMap " << currentLocationMap.x << " : " << currentLocationMap.y;
+   msg.data = ss.str();
+   infoLogPublisher.publish(msg);
+
 
     if (currentMode == 2 || currentMode == 3) { //Robot is in automode
 
@@ -734,8 +739,22 @@ void mapAverage()
             infoLogPublisher.publish(msg);
         }
 
+    bool useCustomTransform = true;
+    if (useCustomTransform)
+    {
+        geometry_msgs::Pose2D offset;
+        offset.x = currentLocation.x - currentLocationMap.x;
+        offset.y = currentLocation.y - currentLocationMap.y;
+
+        centerLocation.x = offset.x - centerLocationMap.x;
+        centerLocation.y = offset.y - centerLocationMap.y;
+
+    }
+    else
+    {
         centerLocation.x = odomPose.pose.position.x; //set centerLocation in odom frame
         centerLocation.y = odomPose.pose.position.y;
+    }
 
     }
 }
@@ -785,7 +804,8 @@ void mapAverage()
 
 /*
    stringstream ss;
-   ss << "map center " << centerLocationMap.x << " : " << centerLocationMap.y << " centerLocation " << centerLocation.x << " : " << centerLocation.y << " currentLlocation " << currentLocation.x << " : " << currentLocation.y << " currentLocationAverage " << currentLocationAverage.x << " : " << currentLocationAverage.y << "curMap " << currentLocationMap.x << " : " << currentLocationMap.y;
+   ss << "map center " << centerLocationMap.x << " : " << centerLocationMap.y << " centerLocation " << centerLocation.x << " : " << centerLocation.y << " currentLlocation " << currentLocation.x << " : " << currentLocation.y << " currentLocationAverage " << currentLocationAverage.x << " : " << currentLocationAverage.y << " curMap " << currentLocationMap.x << " : " << currentLocationMap.y;
    msg.data = ss.str();
    infoLogPublisher.publish(msg);
    */
+
