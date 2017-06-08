efe5c43bfe06eb07896cc39d3f8f8aac4d04109b
Kelsey Geiger <kelseyrgeiger@gail.com>
2017-06-08 16:08:49 -0600

    (UNTESTED) Candidate for final DropoffController refactor.

--- src/mobility/src/DropOffController.cpp
+++ src/mobility/src/DropOffController.cpp
@@ -1,62 +1,46 @@
 #include "DropOffController.h"
 
 DropOffController::DropOffController() {
-    cameraOffsetCorrection = 0.020; //meters
-    centeringTurn = 0.15; //radians
-    seenEnoughCenterTagsCount = 10;
-    collectionPointVisualDistance = 0.5; //in meters
+
     reachedCollectionPoint = false;
-    spinSize = 0.10; //in meters aka 10cm 
-    addSpinSizeAmmount = 0.10; //in meters
 
-    result.cmdVel = 0;
-    result.angleError = 0;
-    result.fingerAngle = -1;
-    result.wristAngle = -1;
-    result.goalDriving = false; //set true for when driving is goal location oriented and false when it is target or time oriented
-    result.centerGoal; //goal that is center location or based upon the center location.
-    result.reset = false;
-    result.timer = false;
-
-    left = false;
-    right = false;
+    result.type = behavior;
+    result.b = init;
 
     circularCenterSearching = false;
     spinner = 0;
     centerApproach = false;
-    timeWithoutSeeingEnoughCenterTags = time(0);
+    lastCenterTagThresholdTime = time(0);
     seenEnoughCenterTags = false;
-    centerSeen = false;
-    timeElapsedSinceTimeSinceSeeingEnoughCenterTags = time(0);
-    circularCenterSearching = false;
     prevCount = 0;
 
-    searchVelocity = 0.15;
+    isPrecisionDriving = false;
+    startWaypoint = false;
+
 }
 
+DropOffController::~DropOffController() {
 
+}
 
-void DropOffController::calculateDecision() {
+Result DropOffController::CalculateResult() {
 
-    result.goalDriving = true; //assumewe are driving to the center unless we see targets or have seen targets.
-    result.timer = false;
-
-
-    //if we are in the routine for exciting the circle once we have droppeda block off and reseting all our flags
+    //if we are in the routine for exciting the circle once we have dropped a block off and reseting all our flags
     //to resart our search.
     if(reachedCollectionPoint)
     {
-        result.goalDriving = false;
-
-        //timerStartTime was reset before we entered reachedCollectionPoint so
-        //we can now use it for our timeing of 2 seconds
-
         if (timerTimeElapsed >= 4)
         {
-            result.reset = true; //tell mobility to reset to search parameters
+            result.type = behavior;
+            result.b = targetReturned;
+            Reset();
+            return result;
         }
         else if (timerTimeElapsed >= 1)
         {
+            isPrecisionDriving = true;
+            result.type = precisionDriving;
+
             //open fingers
             float angle;
             angle = M_PI_2;
@@ -64,36 +48,44 @@ void DropOffController::calculateDecision() {
             angle= 0;
             result.wristAngle = angle; //raise wrist
 
-            result.cmdVel = -0.3;
-            result.angleError = 0.0;
+            result.pd.cmdVel = -0.3;
+            result.pd.cmdError = 0.0;
         }
-        return;
+
+        return result;
     }
+
+    double distanceToCenter = hypot(this->centerLocation.x - this->currentLocation.x, this->centerLocation.y - this->currentLocation.y);
+    int count = countLeft + countRight;
 
     //check to see if we are driving to the center location or if we need to drive in a circle and look.
     if (distanceToCenter > collectionPointVisualDistance && !circularCenterSearching && count == 0) {
 
-        //set angle to center as goal heading
-        result.centerGoal.theta = atan2(centerLocation.y - currentLocation.y, centerLocation.x - currentLocation.x);
+        result.type = waypoint;
+        result.wpts.waypoints.push_back(centerLocation);
+        startWaypoint = false;
+        isPrecisionDriving = false;
 
-        //set center as goal position
-        result.centerGoal.x = centerLocation.x;
-        result.centerGoal.y = centerLocation.y;
-        //spinWasTrue = true; only turn on for random walk to center
     }
     else if (timerTimeElapsed >=5)//spin search for center
     {
+        geometry_msgs::Pose2D nextSpinPoint;
+
         //sets a goal that is 60cm from the centerLocation and spinner
         //radians counterclockwise from being purly along the x-axis.
-        result.centerGoal.x = centerLocation.x + (spinSize + addSpinSize) * cos(spinner);
-        result.centerGoal.y = centerLocation.y + (spinSize + addSpinSize) * sin(spinner);
-        result.centerGoal.theta = atan2(result.centerGoal.y - currentLocation.y, result.centerGoal.x - currentLocation.x);
+        nextSpinPoint.x = centerLocation.x + (initialSpinSize + spinSizeIncrease) * cos(spinner);
+        nextSpinPoint.y = centerLocation.y + (initialSpinSize + spinSizeIncrease) * sin(spinner);
+        nextSpinPoint.theta = atan2(nextSpinPoint.y - currentLocation.y, nextSpinPoint.x - currentLocation.x);
+
+        result.type = waypoint;
+        result.wpts.waypoints.clear();
+        result.wpts.waypoints.push_back(nextSpinPoint);
 
         spinner += 45*(M_PI/180); //add 45 degrees in radians to spinner.
         if (spinner > 2*M_PI)
         {
             spinner -= 2*M_PI;
-	    addSpinSize += addSpinSizeAmmount;
+            spinSizeIncrease += spinSizeIncrement;
         }
         circularCenterSearching = true;
         //safety flag to prevent us trying to drive back to the
@@ -101,20 +93,26 @@ void DropOffController::calculateDecision() {
         //greater than collectionPointVisualDistance from the center.
     }
 
+    bool left = (countLeft > 0);
+    bool right = (countRight > 0);
+    bool centerSeen = (right || left);
 
-    //reset timeWithoutSeeingEnoughCenterTags timout timer to current time
-    if ((!centerApproach && !seenEnoughCenterTags) || (count > 0 && !seenEnoughCenterTags)) timeWithoutSeeingEnoughCenterTags = time(0);
+    //reset lastCenterTagThresholdTime timout timer to current time
+    if ((!centerApproach && !seenEnoughCenterTags) || (count > 0 && !seenEnoughCenterTags)) {
+
+        lastCenterTagThresholdTime = time(0);
+
+    }
 
     if (count > 0 || seenEnoughCenterTags || prevCount > 0) //if we have a target and the center is located drive towards it.
     {
         centerSeen = true;
-        result.goalDriving = false;
 
         if (seenEnoughCenterTags) //if we have seen enough tags
         {
             if ((countLeft-5) > countRight) //and there are too many on the left
             {
-                right = false; //then we say non on the right to cause us to turn right
+                right = false; //then we say none on the right to cause us to turn right
             }
             else if ((countRight-5) > countLeft)
             {
@@ -130,110 +128,89 @@ void DropOffController::calculateDecision() {
 
         //otherwise turn till tags on both sides of image then drive straight
         if (left && right) {
-            result.cmdVel = searchVelocity;
-            result.angleError = 0.0;
+            result.pd.cmdVel = searchVelocity;
+            result.pd.cmdError = 0.0;
         }
         else if (right) {
-            result.cmdVel = -0.1 * turnDirection;
-            result.angleError = -centeringTurn*turnDirection;
+            result.pd.cmdVel = -0.1 * turnDirection;
+            result.pd.cmdError = -centeringTurn*turnDirection;
         }
         else if (left){
-            result.cmdVel = -0.1 * turnDirection;
-            result.angleError = centeringTurn*turnDirection;
+            result.pd.cmdVel = -0.1 * turnDirection;
+            result.pd.cmdError = centeringTurn*turnDirection;
         }
         else
         {
-            result.cmdVel = searchVelocity;
-            result.angleError = 0.0;
+            result.pd.cmdVel = searchVelocity;
+            result.pd.cmdError = 0.0;
         }
 
         //must see greater than this many tags before assuming we are driving into the center and not along an edge.
-        if (count > seenEnoughCenterTagsCount)
+        if (count > centerTagThreshold)
         {
             seenEnoughCenterTags = true; //we have driven far enough forward to be in the circle.
-            timeWithoutSeeingEnoughCenterTags = time(0);
+            lastCenterTagThresholdTime = time(0);
         }
         if (count > 0) //reset gaurd to prevent drop offs due to loosing tracking on tags for a frame or 2.
         {
-            timeWithoutSeeingEnoughCenterTags = time(0);
+            lastCenterTagThresholdTime = time(0);
         }
         //time since we dropped below countGuard tags
-        timeElapsedSinceTimeSinceSeeingEnoughCenterTags = time(0) - timeWithoutSeeingEnoughCenterTags;
+        time_t timeSinceSeeingEnoughCenterTags = time(0) - lastCenterTagThresholdTime;
 
         //we have driven far enough forward to have passed over the circle.
-        if (count == 0 && seenEnoughCenterTags && timeElapsedSinceTimeSinceSeeingEnoughCenterTags > 1) {
+        if (count == 0 && seenEnoughCenterTags && timeSinceSeeingEnoughCenterTags > 1) {
             centerSeen = false;
         }
         centerApproach = true;
         prevCount = count;
         count = 0;
     }
-    //was on approach to center and did not seenEnoughCenterTags
-    //for maxTimeAllowedWithoutSeeingCenterTags seconds so reset.
-    else if (centerApproach) {
-        result.goalDriving = false;
-        int maxTimeAllowedWithoutSeeingCenterTags = 6; //seconds
 
-        timeElapsedSinceTimeSinceSeeingEnoughCenterTags = time(0) - timeWithoutSeeingEnoughCenterTags;
-        if (timeElapsedSinceTimeSinceSeeingEnoughCenterTags > maxTimeAllowedWithoutSeeingCenterTags)
+    //was on approach to center and did not seenEnoughCenterTags
+    //for centerTagSearchCutoff seconds so reset.
+    else if (centerApproach) {
+
+        time_t timeSinceSeeingEnoughCenterTags = time(0) - lastCenterTagThresholdTime;
+        if (timeSinceSeeingEnoughCenterTags > centerTagSearchCutoff)
         {
             //go back to drive to center base location instead of drop off attempt
             reachedCollectionPoint = false;
             seenEnoughCenterTags = false;
             centerApproach = false;
+
+            result.type = waypoint;
+            result.wpts.waypoints.push_back(centerLocation);
+            isPrecisionDriving = false;
         }
         else
         {
-            result.cmdVel = searchVelocity;
-            result.angleError = 0.0;
+            result.pd.cmdVel = searchVelocity;
+            result.pd.cmdError = 0.0;
         }
 
-        right = false;
-        left = false;
-        count = 0;
-        centerSeen = false;
-        return;
+        return result;
 
     }
 
     if (!centerSeen && seenEnoughCenterTags)
     {
         reachedCollectionPoint = true;
-        timerStartTime = time(0);
-        result.goalDriving = false;
         centerApproach = false;
-        result.timer = true;
+        returnTimer = time(0);
     }
 
-    return;
+    return result;
 }
 
-
-void DropOffController::setDataLocations(geometry_msgs::Pose2D center, geometry_msgs::Pose2D current, float sync) {
-
-    centerLocation = center;
-    currentLocation = current;
-    timerTimeElapsed = sync;
-    calculateDecision();
-
-}
-
-
-void DropOffController::reset() {
-    result.cmdVel = 0;
-    result.angleError = 0;
+void DropOffController::Reset() {
+    result.pd.cmdVel = 0;
+    result.pd.cmdError = 0;
     result.fingerAngle = -1;
     result.wristAngle = -1;
-    result.goalDriving = false; //set true for when driving is goal location oriented and false when it is target or time oriented
-    result.centerGoal; //goal that is center location or based upon the center location.
-    result.reset = false;
     spinner = 0;
-    addSpinSize = 0;
+    spinSizeIncrease = 0;
     prevCount = 0;
-
-    left = false;
-    right = false;
-    centerSeen = false;
 
     //reset flags
     reachedCollectionPoint = false;
@@ -242,30 +219,56 @@ void DropOffController::reset() {
 
 }
 
-void DropOffController::setDataTargets(int ccount, double lleft, double rright)
-{
-    count = ccount;
-    if (rright > 0)
-    {
-        right = true;
-    }
-    else
-    {
-        right = false;
-    }
-    if (lleft > 0)
-    {
-        left = true;
-    }
-    else
-    {
-        left = false;
+void DropOffController::UpdateData(const apriltags_ros::AprilTagDetectionArray::ConstPtr& message) {
+    countRight = 0;
+    countLeft = 0;
+
+    if(targetHeld) {
+        // if a target is detected and we are looking for center tags
+        if (message->detections.size() > 0 && !reachedCollectionPoint) {
+
+            // this loop is to get the number of center tags
+            for (int i = 0; i < message->detections.size(); i++) {
+                if (message->detections[i].id == 256) {
+                    geometry_msgs::PoseStamped cenPose = message->detections[i].pose;
+
+                    // checks if tag is on the right or left side of the image
+                    if (cenPose.pose.position.x + cameraOffsetCorrection > 0) {
+                        countRight++;
+
+                    } else {
+                        countLeft++;
+                    }
+                }
+            }
+        }
+
+        if((countLeft + countRight) > 0) {
+            isPrecisionDriving = true;
+        } else {
+            startWaypoint = true;
+        }
     }
 
-    countLeft = lleft;
-    countRight = rright;
 }
 
-DropOffController::~DropOffController() {
+bool DropOffController::ShouldInterrupt() {
+    return startWaypoint;
+}
 
+bool DropOffController::IsChangingMode() {
+    return isPrecisionDriving;
+}
+
+void DropOffController::SetLocationData(geometry_msgs::Pose2D center, geometry_msgs::Pose2D current) {
+    centerLocation = center;
+    currentLocation = current;
+}
+
+void DropOffController::SetTargetPickedUp() {
+    targetHeld = true;
+}
+
+void DropOffController::SetBlockBlockingUltrasound(bool blockBlock) {
+    targetHeld = targetHeld || blockBlock;
 }
--- src/mobility/src/DropOffController.h
+++ src/mobility/src/DropOffController.h
@@ -3,18 +3,9 @@
 #define HEADERFILE_H
 
 #include <geometry_msgs/Pose2D.h>
+#include <apriltags_ros/AprilTagDetectionArray.h>
 #include <std_msgs/Float32.h>
-
-struct DropOffResult {
-    float cmdVel;
-    float angleError;
-    float fingerAngle;
-    float wristAngle;
-    bool goalDriving; //set true for when driving is goal location oriented and false when it is target or time oriented
-    geometry_msgs::Pose2D centerGoal;
-    bool reset;
-    bool timer;
-};
+#include "StandardVars.h"
 
 class DropOffController
 {
@@ -22,61 +13,96 @@ public:
     DropOffController();
     ~DropOffController();
 
-    void reset();
+    void Reset();
+    Result CalculateResult();
+    void UpdateData(const apriltags_ros::AprilTagDetectionArray::ConstPtr& message);
+    bool ShouldInterrupt();
 
-    DropOffResult getState() {return result;}
-    float getSpinner() {return spinner;}
-    float getCentX() { return centerLocation.x;}
-    float getCount() { return count;}
+    bool IsChangingMode();
+    void SetLocationData(geometry_msgs::Pose2D center, geometry_msgs::Pose2D current);
+    void SetTargetPickedUp();
+    void SetBlockBlockingUltrasound(bool blockBlock);
 
-    void setDataTargets(int ccount, double lleft, double rright);
-    void setCenterDist(float dist) {distanceToCenter = dist;}
-    void setDataLocations(geometry_msgs::Pose2D center, geometry_msgs::Pose2D current, float sync);
+    float GetSpinner() {return spinner;}
 
 private:
 
-    bool right;
-    bool left;
-    bool reachedCollectionPoint;
+    //Constants
 
-    //is driving in a circle to find the nest
-    bool circularCenterSearching;
+    const float cameraOffsetCorrection = 0.020; //meters
+    const float centeringTurn = 0.15; //radians
+    const int centerTagThreshold = 10;
+    const int centerTagSearchCutoff = 6; //seconds
+    const float collectionPointVisualDistance = 0.5; //in meters
+    const float initialSpinSize = 0.10; //in meters aka 10cm
+    const float spinSizeIncrement = 0.10; //in meters
+    const float searchVelocity = 0.15; //in meters per second
+
+
+
+    //Instance Variables
+
+    /*
+     *  Timers and Accumulators
+     */
 
     //keep track of progression around a circle when driving in a circle
     float spinner;
 
-    //set to true when we are entering the center circle
+    //Timer for return code (dropping the cube in the center)- used for timerTimeElapsed
+    time_t returnTimer;
+
+    //Time since last exceeding the tag threshold
+    time_t lastCenterTagThresholdTime;
+
+    //Previous tag count
+    int prevCount;
+
+
+    /*
+     *  Cached External Information
+     */
+
+    //Count of tags on the left and right, respectively
+    int countLeft;
+    int countRight;
+
+    //Center and current locations as of the last call to setLocationData
+    geometry_msgs::Pose2D centerLocation;
+    geometry_msgs::Pose2D currentLocation;
+
+    //Time since modeTimer was started, in seconds
+    int timerTimeElapsed;
+
+    //The amount over initialSpinSize we've gotten to
+    float spinSizeIncrease;
+
+    /*
+     *  Flags
+     */
+
+    //Flag indicating that a target has been picked up and is held
+    bool targetHeld;
+
+    //Flag indicating that we're in the center
+    bool reachedCollectionPoint;
+
+    //Flag indicating that we're driving in a circle to find the nest
+    bool circularCenterSearching;
+
+    //Flag for when we are entering the center circle
     bool centerApproach;
 
     //we have seen enough central collection tags to be certain we are either in or driving towards the nest.
     bool seenEnoughCenterTags;
 
-    //central collection point has been seen (aka the nest)
-    bool centerSeen;
+    //Flag to indicate a switch to precision driving
+    bool isPrecisionDriving;
 
-    time_t timeWithoutSeeingEnoughCenterTags;
-    float cameraOffsetCorrection;
-    float centeringTurn;
-    int seenEnoughCenterTagsCount;
-    DropOffResult result;
-    int count;
-    int prevCount;
-    double countLeft;
-    double countRight;
-    float collectionPointVisualDistance;
-    float distanceToCenter;
-    geometry_msgs::Pose2D centerLocation;
-    geometry_msgs::Pose2D currentLocation;
-    float timerTimeElapsed;
-    time_t timerStartTime;
-    float timeElapsedSinceTimeSinceSeeingEnoughCenterTags;
-    float spinSize;
-    float addSpinSize;
-    float addSpinSizeAmmount;
+    //Flag to indicate that we're starting to follow waypoints
+    bool startWaypoint;
 
-    float searchVelocity;
-
-    void calculateDecision();
+    Result result;
 
 
 };
