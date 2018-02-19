947bdeb54091891f9fa9da32fbf58a51b0567544
akil andrews <akil.andrews@gmail.com>
2018-02-19 16:14:18 -0700

    this version was tested during precompetion trial at hodgin hall

--- misc/load_arduino_code.sh
+++ misc/load_arduino_code.sh
@@ -14,9 +14,9 @@ echo "copy calibration values to arduino sketch"
 min_cal=$(echo $calibration_vars | awk '{printf "{%s, %s, %s};", $1, $2, $3}')
 max_cal=$(echo $calibration_vars | awk '{printf "{%s, %s, %s};", $4, $5, $6}') 
 echo $min_cal $max_cal
-sed -i "s/magnetometer_accelerometer.m_min\s*=.*;/magnetometer_accelerometer.m_min = (LSM303::vector<int16_t>)${min_cal}/" Swarmathon_Arduino/Swarmathon_Arduino.ino
-sed -i "s/magnetometer_accelerometer.m_max\s*=.*;/magnetometer_accelerometer.m_max = (LSM303::vector<int16_t>)${max_cal}/" Swarmathon_Arduino/Swarmathon_Arduino.ino
+sed -i "s/magnetometer_accelerometer.m_min\s*=.*;/magnetometer_accelerometer.m_min = (LSM303::vector<int16_t>)${min_cal}/" ~/Swarmathon-Arduino/Swarmathon_Arduino/Swarmathon_Arduino.ino
+sed -i "s/magnetometer_accelerometer.m_max\s*=.*;/magnetometer_accelerometer.m_max = (LSM303::vector<int16_t>)${max_cal}/" ~/Swarmathon-Arduino/Swarmathon_Arduino/Swarmathon_Arduino.ino
 
 #echo "build sketch upload to microcontroller"
-~/arduino-1.6.8/arduino --upload ~/SwarmBaseCode-ROS/misc/Swarmathon_Arduino/Swarmathon_Arduino.ino --port /dev/ttyACM0
+~/arduino-1.6.8/arduino --upload ~/Swarmathon-Arduino/Swarmathon_Arduino/Swarmathon_Arduino.ino --port /dev/ttyACM1
 
