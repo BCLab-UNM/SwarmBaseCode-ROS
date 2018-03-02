8a2cc1d97b7f8596002b6cced32d63219c660132
Matthew Fricke <matthew@fricke.co.uk>
2018-03-02 14:35:43 -0700

    Updated Run Calibration Script ============================== Added @akilandrews script that extracts calibration data, loads it into swarmie_control sketch, and uploads sketch to Arduino.

--- misc/run_calibration.sh
+++ misc/run_calibration.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+# START OF GET ARDUINO PORT FUNCTION
 get_arduino_port () {
 for sysdevpath in $(find /sys/bus/usb/devices/usb*/ -name dev); do
     (
@@ -14,6 +15,61 @@ for sysdevpath in $(find /sys/bus/usb/devices/usb*/ -name dev); do
     )
 done
 }
+# END OF GET ARDUINO PORT FUNTION 
+
+# START OF LOAD CALIBRATION
+load_calibration_data () {
+echo "Locate calibration file and extract values..."
+calibrationFile="../../KSC.cal"
+# e.g. min: { -9999, -9999, -9999 } max: { +9999, +9999, +9999 }"
+calibration_vars=$(awk '/\+/ || /\-/ {print}' $calibrationFile)
+if [ $? -ne 0 ]; then
+	echo "********************************"
+        echo "FAILED TO OPEN CALIBRATION FILE!"
+        exit 1
+fi
+calibration_vars=$(echo $calibration_vars | sed 's/}/ /g; s/{/ /g; s/,/ /g; s/min:/ /; s/max:/ /;')
+echo $calibration_vars
+
+echo "Copy calibration values to arduino sketch..."
+min_cal=$(echo $calibration_vars | awk '{printf "{%s, %s, %s};", $1, $2, $3}')
+max_cal=$(echo $calibration_vars | awk '{printf "{%s, %s, %s};", $4, $5, $6}') 
+arduino_sketch=../arduino/swarmie_control/swarmie_control.ino
+sed -i "s/magnetometer_accelerometer.m_min\s*=.*;/magnetometer_accelerometer.m_min = (LSM303::vector<int16_t>)${min_cal}/" $arduino_sketch
+if [ $? -ne 0 ]; then
+	echo "*********************************************"
+	echo "FAILED TO WRITE CALIBRATION VALUES TO SKETCH!"
+	exit 1
+fi
+sed -i "s/magnetometer_accelerometer.m_max\s*=.*;/magnetometer_accelerometer.m_max = (LSM303::vector<int16_t>)${max_cal}/" $arduino_sketch
+if [ $? -ne 0 ]; then
+	echo "*********************************************"
+	echo "FAILED TO WRITE CALIBRATION VALUES TO SKETCH!"
+	exit 1
+fi
+
+echo "Build sketch and upload to arduino leonardo..."
+arduino_port=$(get_arduino_port)
+leo_cmd="--upload $arduino_sketch"
+leo_pref="--preserve-temp-files"
+leo_pref="$leo_pref --pref serial.port=$arduino_port"
+leo_pref="$leo_pref --pref build.verbose=true"
+leo_pref="$leo_pref --pref upload.verbose=true"
+leo_pref="$leo_pref --pref build.path=/tmp"
+leo_pref="$leo_pref --pref sketchbook.path=../arduino/"
+leo_pref="$leo_pref --pref board=leonardo"
+~/arduino-1.8.5/arduino $leo_cmd $leo_pref
+if [ $? -ne 0 ]; then
+	echo "*********************************************"
+	echo "FAILED TO UPLOAD CALIBRATION FILE TO ARDUINO!"
+	exit 1
+fi
+echo "***************************"
+echo "CALIBRATION UPLOAD SUCCESS!"
+}
+# END OF LOAD CALIBRATION FUNCTION
+
+
 
 arduino_port=$(get_arduino_port)
 if [ -z $arduino_port ]; then
@@ -35,4 +91,5 @@ sleep 1
 echo "Starting Calibration"
 python calibrate.py
 pkill roscore
+load_calibration_data
 exit 0
