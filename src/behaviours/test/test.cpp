07c0f88b64149ec3b51a6565244889f06af8d28d
Will Vining <wfvining@gmail.com>
2018-06-18 13:04:15 -0600

    Moved headers to an include directory

--- src/behaviours/test/test.cpp
+++ src/behaviours/test/test.cpp
@@ -12,3 +12,77 @@ TEST(BehaviorManager, noBehaviors) {
    EXPECT_EQ(WristControl::DOWN, a.wrist);
    EXPECT_EQ(GripperControl::OPEN, a.grip);
 }
+
+TEST(BehaviorManager, constantBehavior) {
+   BehaviorManager manager;
+   Action action;
+   action.drive.left = -100;
+   action.drive.right = 100;
+   action.wrist = WristControl::UP;
+   action.grip  = GripperControl::CLOSED;
+   Constant c(action);
+   manager.RegisterBehavior(&c);
+   Action a = manager.NextAction();
+   EXPECT_EQ(a.drive.left, action.drive.left);
+   EXPECT_EQ(a.drive.right, action.drive.right);
+   EXPECT_EQ(a.wrist, action.wrist);
+   EXPECT_EQ(a.grip, action.grip);
+}
+
+TEST(Constant, constantBehavior) {
+   Action action;
+   action.drive.left = -100;
+   action.drive.right = 100;
+   action.wrist = WristControl::UP;
+   action.grip  = GripperControl::CLOSED;
+   Constant c(action);
+   Action a = c.GetAction();
+   EXPECT_EQ(a.drive.left, action.drive.left);
+   EXPECT_EQ(a.drive.right, action.drive.right);
+   EXPECT_EQ(a.wrist, action.wrist);
+   EXPECT_EQ(a.grip, action.grip);
+}
+
+TEST(Constant, constantBehaviorNoChangeAfterUpdate) {
+   Action action;
+   action.drive.left = -100;
+   action.drive.right = 100;
+   action.wrist = WristControl::UP;
+   action.grip  = GripperControl::CLOSED;
+   Constant c(action);
+   c.Update();
+   Action a = c.GetAction();
+   EXPECT_EQ(a.drive.left, action.drive.left);
+   EXPECT_EQ(a.drive.right, action.drive.right);
+   EXPECT_EQ(a.wrist, action.wrist);
+   EXPECT_EQ(a.grip, action.grip);   
+}
+
+TEST(Constant, constantBehaviorNoChangeAfterLLActionUpdate) {
+   Action action;
+   action.drive.left = -100;
+   action.drive.right = 100;
+   action.wrist = WristControl::UP;
+   action.grip  = GripperControl::CLOSED;
+   Constant c(action);
+   Action action2;
+   action2.drive.left = 0;
+   action2.drive.right = 10;
+   action2.wrist = WristControl::DOWN_2_3;
+   action2.grip = GripperControl::OPEN;
+   c.SetLowerLevelAction(action2);
+   c.Update();
+   Action a = c.GetAction();
+   EXPECT_EQ(a.drive.left, action.drive.left);
+   EXPECT_EQ(a.drive.right, action.drive.right);
+   EXPECT_EQ(a.wrist, action.wrist);
+   EXPECT_EQ(a.grip, action.grip);   
+}
+
+int main(int argc, char** argv)
+{
+   testing::InitGoogleTest(&argc, argv);
+   ros::init(argc, argv, "tester");
+   ros::NodeHandle nh;
+   return RUN_ALL_TESTS();
+}
