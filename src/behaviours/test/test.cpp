a3672c613335f9d9efdf91f8bf226641419616b3
Will Vining <wfvining@gmail.com>
2018-06-20 17:31:12 -0600

    Additional tests for SwarmieSensors tag detection

--- src/behaviours/test/test.cpp
+++ src/behaviours/test/test.cpp
@@ -3,6 +3,15 @@
 #include <gtest/gtest.h>
 #include <boost/math/quaternion.hpp>
 
+/**
+ * Simple hash function to deterministically create many different tag
+ * IDs.
+ */
+int simple_hash(int x)
+{
+   return (1333333 * x) >> (32 - 16);
+}
+
 TEST(BehaviorManager, noBehaviors) {
    // The default behavior is to sit and do nothing after lowering and
    // opening the gripper.
@@ -96,12 +105,33 @@ TEST(SwarmieSensors, initialTagDetections) {
    EXPECT_EQ(0, sensors.GetTags().size());
 }
 
-TEST(SwarmieSensors, setTags) {
+TEST(SwarmieSensors, setTagsOne) {
    SwarmieSensors sensors;
-   Tag t(0, 0, 0, 0, boost::math::quaternion<double>(1.2, 1.2, 1.2, 5.2));
+   boost::math::quaternion<double> q(1.2, 1.2, 1.2, 5.2);
+   Tag t(1, 2, 3, 4, q);
    sensors.DetectedTag(t);
    std::vector<Tag> tags = sensors.GetTags();
    EXPECT_EQ(1, sensors.GetTags().size());
+   Tag t_out = sensors.GetTags()[0];
+   EXPECT_EQ(q, sensors.GetTags()[0].GetOrientation());
+   EXPECT_EQ(t.GetX(), t_out.GetX());
+   EXPECT_EQ(t.GetY(), t_out.GetY());
+   EXPECT_EQ(t.GetZ(), t_out.GetZ());
+}
+
+TEST(SwarmieSensors, setTagsMany)
+{
+   SwarmieSensors sensors;
+   boost::math::quaternion<double> q(0.2, 10.4, -1.2, 0.3);
+   std::vector<Tag> tags_in;
+   for(int i = 0; i <= 1024; i++)
+   {
+      Tag t(simple_hash(i), i, i, i, q);
+      sensors.DetectedTag(t);
+      tags_in.push_back(t);
+   }
+   EXPECT_EQ(tags_in.size(), sensors.GetTags().size());
+   // TODO: count the number of tags with each ID and ensure that they match
 }
 
 int main(int argc, char** argv)
