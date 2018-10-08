673f6f85ec5a15ebf7b1238cce92ffe182f64617
Kristiana Rendon <rendonk@unm.edu>
2018-10-08 15:30:50 -0600

    Updated Files To Match Style Guide Fixed spacing per indentation to 2 spaces, function/class/statements "{" and "}" are on their own line, variables changed to COBOL case, fixed a few typos and one variable name (skidsteer_publisher) to be consistent with other publisher variable names.

--- src/sbridge/src/main.cpp
+++ src/sbridge/src/main.cpp
@@ -5,34 +5,39 @@ using namespace std;
 
 void sigintEventHandler(int signal);
 
-int main(int argc, char **argv) {
-    sleep(10);
+int main(int argc, char **argv)
+{
+  sleep(10);
 
-    char host[128];
-    gethostname(host, sizeof (host));
-    string hostname(host);
-    string publishedName;
-    
-    if (argc >= 2) {
-        publishedName = argv[1];
-        cout << "Welcome to the world of tomorrow " << publishedName << "!  ABridge module started." << endl;
-    } else {
-        publishedName = hostname;
-        cout << "No Name Selected. Default is: " << publishedName << endl;
-    }
+  char host[128];
+  gethostname(host, sizeof (host));
+  string hostname(host);
+  string published_name;
 
-    ros::init(argc, argv, (hostname + "_SBRIDGE"), ros::init_options::NoSigintHandler);
+  if (argc >= 2)
+  {
+      published_name = argv[1];
+      cout << "Welcome to the world of tomorrow " << published_name << "!  ABridge module started." << endl;
+  }
+  else
+  {
+      published_name = hostname;
+      cout << "No Name Selected. Default is: " << published_name << endl;
+  }
 
-    sbridge sb(publishedName);
+  ros::init(argc, argv, (hostname + "_SBRIDGE"), ros::init_options::NoSigintHandler);
 
-    signal(SIGINT, sigintEventHandler);
+  sbridge sb(published_name);
 
-    ros::spin();
+  signal(SIGINT, sigintEventHandler);
+
+  ros::spin();
 
 	return EXIT_SUCCESS;
 }
 
-void sigintEventHandler(int signal) {
-    ros::shutdown();
+void sigintEventHandler(int signal)
+{
+  ros::shutdown();
 }
 
