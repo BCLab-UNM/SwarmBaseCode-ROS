6f1c7e6f8f11911bb6d6dac84e99659aef8d7122
Carter Frost <carter90@gmail.com>
2018-11-20 16:11:43 -0800

    Allows run.sh to be executed from any folder within the repo using catkin locate and outside using dirname
    Testing:
    From home you could run `./SwarmBaseCode-ROS/run.sh` this will use dirname
    From SwarmBaseCode-ROS/src/behaviours/src you could run it as ../../../run.sh this will use catkin locate

--- run.sh
+++ run.sh
@@ -1,18 +1,19 @@
 #!/bin/bash
-echo "Running in $PWD" 
 previous_gazebo_model_path=${GAZEBO_MODEL_PATH}
 previous_gazebo_plugin_path=${GAZEBO_PLUGIN_PATH}
-export SWARMATHON_APP_ROOT="$PWD"
-export GAZEBO_MODEL_PATH="$PWD/simulation/models"
-export GAZEBO_PLUGIN_PATH="$PWD/build/gazebo_plugins"
-source "$PWD/devel/setup.bash"
+export SWARMATHON_APP_ROOT="$(catkin locate)"
+if [ -z "$t" ]; then SWARMATHON_APP_ROOT=$(dirname "$0"); fi
+echo "Running from: $PWD with repo root: $SWARMATHON_APP_ROOT"
+export GAZEBO_MODEL_PATH="$SWARMATHON_APP_ROOT/simulation/models"
+export GAZEBO_PLUGIN_PATH="$SWARMATHON_APP_ROOT/build/gazebo_plugins"
+source "$SWARMATHON_APP_ROOT/devel/setup.bash"
 echo Cleaning up ROS and Gazebo Processes
 
 #Delete the rqt cache - can take 24 hours for changes in the UI
 # to show up otherwise
 rm ~/.config/ros.org/rqt_gui.ini
 
-./cleanup.sh
+$SWARMATHON_APP_ROOT/cleanup.sh
 echo Killing rosmaster
 pkill rosmaster
 echo Killing roscore
@@ -28,7 +29,7 @@ echo Killing rosmaster
 pkill rosmaster
 echo Killing roscore
 pkill roscore
-./cleanup.sh
+$SWARMATHON_APP_ROOT/cleanup.sh
 # Restore previous environment
 export GAZEBO_MODEL_PATH=$previous_gazebo_model_path
 export GAZEBO_PLUGIN_PATH=$previous_gazebo_plugin_path
