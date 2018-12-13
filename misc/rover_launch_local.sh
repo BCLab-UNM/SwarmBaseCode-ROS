cfbabbce7bb66a0fddf3d8fbaf27bf66fee66093
Abby Pribisova <abbypribis@gmail.com>
2018-12-13 14:16:44 -0700

    Added frame rate to launch local

--- misc/rover_launch_local.sh
+++ misc/rover_launch_local.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
+OPTSTRING=`getopt -l frame-rate: -- $0 $@`
+frame_rate=10
+eval set -- "$OPTSTRING"
+while true
+do
+    case "$1" in
+        --frame-rate ) frame_rate="$2"; shift 2 ;;
+        -- ) shift ; break ;;
+    esac
+done
 roscore &
 sleep 10 # give roscore enough time to start
-./rover_onboard_node_launch.sh localhost $1
+./rover_onboard_node_launch.sh --frame-rate $frame_rate localhost $1
